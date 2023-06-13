7ebc4a7e698baa994321ccbf1ad29eae5d3b25ea
Tri Tran <tri.tranthien@hdwebsoft.dev>
2023-06-13 15:06:41 +0700

    first push

---
+++ js/content.js
@@ -0,0 +1,36 @@
+let count = 0;
+let it0 = setInterval(() => {
+    const shows = document.querySelectorAll('div[data-testid="sr-settings-about"]');
+    if (shows && shows.length > 0) {
+        clearInterval(it0);
+        shows.forEach((element,index) => {
+            if (index < 6) {
+                return;
+            }
+            const btnText = `<button class="local-hide">hide</button>`;
+            element.insertAdjacentHTML('beforebegin',btnText);
+        });
+        const localHideBtns = document.querySelectorAll('.local-hide');
+        localHideBtns.forEach((e,i)=>{
+            const parent = e.closest('div');
+            const hide = parent.querySelector('div[data-testid="sr-settings-content"]');
+            hide.setAttribute('data-is-hidden',1);
+            hide.style.display = 'none';
+            e.addEventListener('click', () => {
+                let isHide = hide.getAttribute('data-is-hidden');
+                if (!isHide) {
+                    hide.setAttribute('data-is-hidden',1);
+                    hide.style.display = 'none';
+                } else {
+                    hide.removeAttribute('data-is-hidden');
+                    hide.style.display = 'block';
+                }
+            })
+        })
+    }
+    count++;
+    console.log(`turn-${count}`);
+}, 1000);
+setTimeout(() => {
+    clearInterval(it0);
+}, 15000);
