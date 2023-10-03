6f9a572586e2af37690960bebb3af845f86135ac
Tan Luu <tan.luuthanh@hdwebsoft.dev>
2023-10-03 09:57:06 +0700

    Add unlock ifram shopify

--- js/content.js
+++ js/content.js
@@ -1,51 +1,76 @@
 let count = 0;
+let currentPage = document.location.href;
+var checkBoxArray = ["vend", "unlockShopify", "vendAge"];
 let it0 = setInterval(() => {
-  chrome.storage.sync.get(["testStorage"]).then((result) => {
-    if (result.testStorage) {
-      runScript();
-    } else {
-      clearInterval(it0);
-    }
-  });
+    checkBoxArray.forEach(id => {
+        chrome.storage.sync.get([id]).then((result) => {
+            if (result[id]) {
+                switch (id) {
+                    case "vend":
+                        runScript();
+                        break;
+                    case "unlockShopify":
+                        unlockShopify();
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                clearInterval(it0);
+            }
+        });
+    });
 }, 1000);
+
 setTimeout(() => {
-  clearInterval(it0);
+    clearInterval(it0);
 }, 15000);
+
 function runScript() {
-  const shows = document.querySelectorAll(
-    'div[data-testid="sr-settings-about"]'
-  );
-  if (shows && shows.length) {
-    shows.forEach((element, index) => {
-      if (index < 6) {
-        return;
-      }
-      const btnText = `<button class="local-hide vd-btn vd-btn--do">Hide</button>`;
-      element.insertAdjacentHTML("beforebegin", btnText);
-    });
-    const localHideBtns = document.querySelectorAll(".local-hide");
-    if (localHideBtns && localHideBtns.length > 0) {
-      clearInterval(it0);
-    }
-    localHideBtns.forEach((e, i) => {
-      const parent = e.closest("div");
-      const hide = parent.querySelector(
-        'div[data-testid="sr-settings-content"]'
-      );
-      hide.setAttribute("data-is-hidden", 1);
-      hide.style.display = "none";
-      e.addEventListener("click", () => {
-        let isHide = hide.getAttribute("data-is-hidden");
-        if (!isHide) {
-          hide.setAttribute("data-is-hidden", 1);
-          hide.style.display = "none";
-        } else {
-          hide.removeAttribute("data-is-hidden");
-          hide.style.display = "block";
+    const shows = document.querySelectorAll(
+        'div[data-testid="sr-settings-about"]'
+    );
+    if (shows && shows.length) {
+        shows.forEach((element, index) => {
+            if (index < 6) {
+                return;
+            }
+            const btnText = `<button class="local-hide vd-btn vd-btn--do">Hide</button>`;
+            element.insertAdjacentHTML("beforebegin", btnText);
+        });
+        const localHideBtns = document.querySelectorAll(".local-hide");
+        if (localHideBtns && localHideBtns.length > 0) {
+            clearInterval(it0);
         }
-      });
-    });
-  }
-  count++;
-  console.log(`turn-${count}`);
+        localHideBtns.forEach((e, i) => {
+            const parent = e.closest("div");
+            const hide = parent.querySelector(
+                'div[data-testid="sr-settings-content"]'
+            );
+            hide.setAttribute("data-is-hidden", 1);
+            hide.style.display = "none";
+            e.addEventListener("click", () => {
+                let isHide = hide.getAttribute("data-is-hidden");
+                if (!isHide) {
+                    hide.setAttribute("data-is-hidden", 1);
+                    hide.style.display = "none";
+                } else {
+                    hide.removeAttribute("data-is-hidden");
+                    hide.style.display = "block";
+                }
+            });
+        });
+    }
+    count++;
+    console.log(`turn-${count}`);
+}
+
+function unlockShopify() {
+    let pageUrl = "shopify"
+    if (currentPage.search(pageUrl) != -1) {
+        let rms = document.querySelectorAll('iframe');
+        rms.forEach(e => {
+            e.style.position = "initial"
+        });
+    }
 }
--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,19 @@
-function onCheckClick() {
-    var value = document.querySelector('#test').checked;
-    chrome.storage.sync.set({ testStorage: value })
+var checkBoxArray = ["vend", "unlockShopify", "vendAge"];
+function onCheckClick(paramId) {
+    var value = document.querySelector("#" + paramId).checked;
+    var data = {}
+    data[paramId] = value
+    chrome.storage.sync.set(data)
 }
-var x = "234";
-chrome.storage.sync.get(["testStorage"]).then((result) => {
-    document.querySelector('#test').checked = result.testStorage
+
+checkBoxArray.forEach(pramId => {
+    document.getElementById(pramId).addEventListener("change", () => { onCheckClick(pramId) });
+    chrome.storage.sync.get([pramId]).then((result) => {
+        document.querySelector("#" + pramId).checked = result[pramId]
+    });
 });
-document.getElementById("test").addEventListener("change", onCheckClick);
+
+
 document.getElementById("generate-vend-age").addEventListener("click", function () {
     const onlyInputs = document.querySelectorAll('#form-vend-age input');
     data = [];
@@ -20,12 +27,21 @@ document.getElementById("generate-vend-age").addEventListener("click", function 
 
     [expired, lastName, firstName, birthday, dlNumber] = data;
     let dlBarcode = '636015080001DL00310300DLDCACDDAFDDB10102016DCBNONEDCDNONEDBA' + expired +
-    'DCS' + lastName + 'DDENDAC' + firstName + 
-    'DDFNDADJASEDDGNDBD03162017DBB' + birthday + 
-    'DBC1DAYBRODAZBRODAU070 INDAW160DCLWDDJ03212018DDK1DAG17501 REGATTA VIEW DRDAIJONESTOWNDAJTXDAK786454636  DCK' + dlNumber + 
-    '  20170317DAQ' + dlNumber;
-
+        'DCS' + lastName + 'DDENDAC' + firstName +
+        'DDFNDADJASEDDGNDBD03162017DBB' + birthday +
+        'DBC1DAYBRODAZBRODAU070 INDAW160DCLWDDJ03212018DDK1DAG17501 REGATTA VIEW DRDAIJONESTOWNDAJTXDAK786454636  DCK' + dlNumber +
+        '  20170317DAQ' + dlNumber;
     if (missingData) {
         alert(dlBarcode);
+    }
+});
+
+document.getElementById('vendAge').addEventListener("change", () => {
+    let form = document.getElementById('div-vend-age');
+    let displayCurrent = form.style.display;
+    if (displayCurrent == 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
     }
 });
